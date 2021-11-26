8d837792757a811baf7527b399057320ef56beee
tanzumt <89188092+tanzumt@users.noreply.github.com>
2021-11-26 18:11:08 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace runtime
     {
         static void Main(string[] args)
         {
-            string port = Environment.GetEnvironmentVariable("PORT");
+            string port = 8080;
             TcpListener server = new TcpListener(IPAddress.Any, Int32.Parse(port));
 
             server.Start();
